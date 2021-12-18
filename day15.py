4b7d3548dbea4ff0d5ebc580cb20a7d86d93e720
jaredblack <jrdblck@gmail.com>
2021-12-17 18:01:15 -0800

    Solution day 15 part A

---
+++ day15.py
@@ -0,0 +1,87 @@
+from math import inf
+
+
+def get_input():
+    in_file = open('test_in15.txt', 'r')
+    lines = [line for line in in_file.read().split('\n')]
+    matrix = [[int(entry) for entry in line] for line in lines]
+    in_file.close()
+
+    return matrix
+
+
+def edge_weight(matrix, u, v):
+    if u == (v[0] - 1, v[1]) or\
+        u == (v[0] + 1, v[1]) or\
+        u == (v[0], v[1] - 1) or\
+        u == (v[0], v[1] + 1):
+        return matrix[v[0]][v[1]]
+    return inf
+
+def in_bounds(matrix, i, j):
+    if i < 0 or i >= len(matrix) or j < 0 or j >= len(matrix[0]):
+        return False
+    return True
+
+def make_big_matrix(small_matrix):
+    matrix = [row * 5 for row in small_matrix]
+    matrix *= 5
+    pass
+
+
+def solve_a(matrix):
+    S = set()
+    Lv = {}
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            Lv[(i,j)] = inf
+    Lv[(0,0)] = 0
+    while len(S) < len(matrix) * len(matrix[0]):
+        minimum = inf
+        for vertex in Lv:
+            if vertex not in S:
+                if Lv[vertex] < minimum:
+                    minimum = Lv[vertex]
+                    u = vertex
+        S.add(u)
+        for i in range(u[0]-1,u[0]+2):
+            for j in range(u[1]-1,u[1]+2):
+                if in_bounds(matrix, i, j) and (i,j) not in S:
+                    if Lv[u] + edge_weight(matrix, u, (i, j)) < Lv[(i,j)]:
+                        Lv[(i,j)] = Lv[u] + edge_weight(matrix, u, (i,j))
+
+    # for k in Lv:
+    #     print(f'{k}: {Lv[k]}')
+    return Lv[(len(matrix)-1,len(matrix[0])-1)]
+
+def solve_b(matrix):
+    S = set()
+    Lv = {}
+    make_big_matrix(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            Lv[(i,j)] = inf
+    Lv[(0,0)] = 0
+    while len(S) < len(matrix) * len(matrix[0]):
+        minimum = inf
+        for vertex in Lv:
+            if vertex not in S:
+                if Lv[vertex] < minimum:
+                    minimum = Lv[vertex]
+                    u = vertex
+        S.add(u)
+        for i in range(u[0]-1,u[0]+2):
+            for j in range(u[1]-1,u[1]+2):
+                if in_bounds(matrix, i, j) and (i,j) not in S:
+                    if Lv[u] + edge_weight(matrix, u, (i, j)) < Lv[(i,j)]:
+                        Lv[(i,j)] = Lv[u] + edge_weight(matrix, u, (i,j))
+        if u == (len(matrix) - 1, len(matrix[0]) - 1):
+            print('point reached')
+            # break
+    return Lv[(len(matrix)-1,len(matrix[0])-1)]
+
+
+
+if __name__ == '__main__':
+    input = get_input()
+    print(solve_b(input))
